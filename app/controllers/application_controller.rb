9b2471169bc972a9d510326bd3ae034b6cc19732
yktksgr <ymmt.yshr1006@gmail.com>
2023-07-11 23:04:11 +0900

    ユーザー認証の再設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'guest' && password == '230701'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-  
+
 end
